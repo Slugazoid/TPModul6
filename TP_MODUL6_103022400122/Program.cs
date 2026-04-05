ca3d1e3df076cac9c04320fe2077b6ae307d4d43
Albert Sebastian <albertsebastian2301@gmail.com>
2026-04-05 22:34:59 +0700

    first commit

---
+++ TP_MODUL6_103022400122/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MODUL6_103022400122
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
